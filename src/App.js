eafa901974fb095f97b8f081d5cc4a1c6300801d
Lucas <lucas.capanelli.es@gmail.com>
2020-04-24 19:12:59 -0300

    Ui implementations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// v1 STAR MATCH - Starting Template - static version
+
+const StarsDisplay = props => (
+  <>
+    { utils.range(1, props.count).map(starId =>
+      <div key={ starId } className="star" />
+    )} 
+  </>
+);
+
+const ButtonNumber = props => (
+  <button 
+    className="number" 
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => console.log( 'Num', props.number )}
+    >
+    { props.number }
+  </button> 
+);
+
+const StarMatch = () => {
+  const [stars, setStars] = React.useState(utils.random(1, 9)); 
+  const [availableNums, setAvailableNums] = React.useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = React.useState([]); 
+  
+  const candidatesWrong = utils.sum(candidateNums) > stars;  
+
+  const numberStatus = (number) => {
+    if (!availableNums.includes(number)) {
+      return 'used'; 
+    }
+    if (candidateNums.includes(number)) {
+      return candidatesWrong ? 'worng' : 'candidate'; 
+    }
+    return 'available'; 
+  }; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          { utils.range(1, stars).map(starId =>
+            <StarsDisplay count={ stars } />
+          )}
+        </div>
+        <div className="right">
+          { utils.range(1, 9).map(number =>
+            <ButtonNumber 
+            key={ number } 
+            status={ numberStatus(number) }
+            number={ number } 
+            />
+          )}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>
   );
-}
+};
 
-export default App;
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
+export default StarMatch;
